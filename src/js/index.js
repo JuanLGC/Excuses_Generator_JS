bcf07efa6379e412c96e7d45f042133a612338cf
JuanLGC <juangcardinale@gmail.com>
2020-07-21 10:17:36 +0000

    Este es el generador de excusas, aun falta practica, me he ayudado mucho del tutorial :(

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,43 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let subject = [
+    "Mi novia",
+    "Donald Trump",
+    "El negrito de la esquina",
+    "Un dios noruego"
+  ];
+  let action = ["vomito en", "cambio por drogas", "se comio", "me robo"];
+  let object = [
+    "mis llaves",
+    "mis ganas de trabajar",
+    "mi voluntad de vivir",
+    "mi proyecto"
+  ];
+  let where = [
+    "en la yapla",
+    "en el palacio de Miraflores",
+    "en Suiza",
+    "en otro plano espiritual"
+  ];
+
+  let subjectindex = Math.floor(Math.random() * 4);
+  let actionindex = Math.floor(Math.random() * 4);
+  let objectindex = Math.floor(Math.random() * 4);
+  let whereindex = Math.floor(Math.random() * 4);
+
+  return (
+    subject[subjectindex] +
+    " " +
+    action[actionindex] +
+    " " +
+    object[objectindex] +
+    " " +
+    where[whereindex]
+  );
 };
